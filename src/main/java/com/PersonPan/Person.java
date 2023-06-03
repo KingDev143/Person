9f76c74139889949bb0f9efb762a920d6c44296b
KingDev143 <kmsdevarajkms@example.com>
2023-06-03 12:05:07 +0530

    hi

--- src/main/java/com/PersonPan/Person.java
+++ src/main/java/com/PersonPan/Person.java
@@ -1,5 +1,52 @@
 package com.PersonPan;
 
-public class Person {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-}
+@Entity
+public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	 private long phno;
+	 private String address;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", phno=" + phno + ", address=" + address + "]";
+	}
+	
+
+	}
+
+	
+	
+
+
